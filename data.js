c97e96c27546e52271bb7c356f0df9c641733f32
James Thomson <52462116+thomsonj-sheffcol@users.noreply.github.com>
2021-01-20 17:14:55 +0000

    Add files via upload

---
+++ data.js
@@ -0,0 +1,177 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sam_100_0036",
+      "name": "SAM_100_0036",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": -1.3337795801064551,
+        "pitch": -0.15256014995781797,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.7881664230419094,
+          "pitch": -0.14343378162370612,
+          "title": "Water",
+          "text": "The academy rooms have water, fruit, snacks and plants - just like a normal office"
+        },
+        {
+          "yaw": -0.4213725545993441,
+          "pitch": -0.28340631393701443,
+          "title": "Dual Time",
+          "text": "WANdisco is a global company with bases across the world so we have included the time at their office in San Ramon just in case they call (which they do)"
+        },
+        {
+          "yaw": -0.9636306107558852,
+          "pitch": 0.2564426007327114,
+          "title": "Flexible tables",
+          "text": "The tables in the academy rooms are all segmented and so can be split and used in many different ways to reflect the flexible way we work."
+        },
+        {
+          "yaw": 0.5917712829434691,
+          "pitch": -0.273450963347603,
+          "title": "Castable TVs",
+          "text": "There are 2 TVs in each academy room and these can be cast to by any of the computers in the room"
+        },
+        {
+          "yaw": -2.2034501222501746,
+          "pitch": 0.12218574445115493,
+          "title": "IT in a Tin",
+          "text": "The academy rooms have both desktop and laptop options so you can learn in a fixed or flexible environment."
+        },
+        {
+          "yaw": -1.8089291338242823,
+          "pitch": -0.08928539907781285,
+          "title": "Door Security",
+          "text": "The academy rooms use swipe access so you can use the room at any time."
+        },
+        {
+          "yaw": -1.4830850143608938,
+          "pitch": -0.17127812668292108,
+          "title": "Transparent board",
+          "text": "Instead of a whiteboard we have a movable glass screen, great for project work"
+        },
+        {
+          "yaw": 2.553204169642685,
+          "pitch": 0.21242124038473165,
+          "title": "Flexible seating",
+          "text": "In the academy rooms we have seating to suit all projects. The wide based swivel chairs are super comfy and we also have sofa style seating."
+        }
+      ]
+    },
+    {
+      "id": "1-sam_100_0027",
+      "name": "SAM_100_0027",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": -0.3017635886221388,
+        "pitch": -0.06446236170835462,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.8558449121609257,
+          "pitch": 0.007235622838489775,
+          "title": "The Dolls House",
+          "text": "The Dolls House has a serious role to play in Computing. We use this for Unit 19 - The Internet of Things. The house currently has a lighting system that can be controlled by a mobile phone and a burglar alarm."
+        },
+        {
+          "yaw": -2.187763273171303,
+          "pitch": -0.121195810580776,
+          "title": "The Server Rack",
+          "text": "We have our own server that we can connect to internally. This can run virtual machines that can be used for network management and software installation across a variety of operating systems"
+        },
+        {
+          "yaw": 0.1952087008923158,
+          "pitch": -0.1332385343725111,
+          "title": "Sawyer the Cobot",
+          "text": "Sawyer is a Cobot, a Robot that can be taught by demonstrating movement as well as programming it. Sawyer has a face and eyes that follow his every move. He even smiles and frowns."
+        },
+        {
+          "yaw": 1.062058224442577,
+          "pitch": -0.0977430533359307,
+          "title": "Water",
+          "text": "Yes, the academy rooms have water which you can help yourself to at any time. We even have a weekly delivery of fruit and snacks"
+        },
+        {
+          "yaw": 2.103615663165831,
+          "pitch": 0.19550514729180613,
+          "title": "IT in a Tin",
+          "text": "As well as high specification desktop machines we have similar specification laptops for you to sit where you want in the room."
+        },
+        {
+          "yaw": 2.8761936979270155,
+          "pitch": 0.009545287728027319,
+          "title": "Castable TVs",
+          "text": "There is no 'tutor station' in the academy rooms as the TVs at either end of the room can be cast to by any of the computers. This is not only great for flexible teaching but learners can use this facility for groupwork."
+        },
+        {
+          "yaw": -1.3849439201574256,
+          "pitch": 0.02912089204484758,
+          "title": "In need of repair?",
+          "text": "We have a selection of old machines and PC / laptop parts for you to learn how to build and repair,"
+        },
+        {
+          "yaw": -2.483143627820567,
+          "pitch": 0.04288288155764164,
+          "title": "Door security",
+          "text": "The academy rooms are swipe card entry and are availabe to academy students at any time"
+        },
+        {
+          "yaw": 0.16265212230816672,
+          "pitch": 0.23675231793960982,
+          "title": "Flexible seating",
+          "text": "The seating in the Millgate room reflects the work going on in here. There are comfy swivel chairs, sofa seating and standing technician's stools."
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
